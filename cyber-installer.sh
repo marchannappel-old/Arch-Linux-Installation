0e76098524244912477b9d8a140aed5fc337a6a6
Marc Hannappel <marc.hannappel@lise.de>
2022-10-18 15:07:31 +0200

    New changes

--- cyber-installer.sh
+++ cyber-installer.sh
@@ -10,8 +10,14 @@ if [[ -z "$DEVICE_SYSTEM"]]; then
     echo "Please choose one of the available system types"
 elif [[ "$DEVICE_SYSTEM" == "EFI" ]]; then
     partitionDiskEFI
+
+    createFsEFI
 elif [[ "$DEVICE_SYSTEM" == "BIOS" ]]; then
     partitionDiskBIOS
+
+    createFsBIOS
 else
     partitionDiskEFI
+
+    createFsEFI
 fi
